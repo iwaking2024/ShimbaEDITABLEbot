3907da08f8238859673dacd3f2dee3e5e1836974
iwaking2024 <149971570+iwaking2024@users.noreply.github.com>
2024-03-27 13:25:49 -0600

    Add files via upload

---
+++ plugins/herramientas-topdf.js
@@ -0,0 +1,13 @@
+import uploadImage from '../lib/uploadImage.js'
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '*[❗] responder / etiquetar una imagen *'    
+let img = await q.download?.()
+let url = await uploadImage(img)    
+let docname = text ? text : m.pushName || 'documento'
+conn.sendFile(m.chat, `http://api.lolhuman.xyz/api/convert/imgtopdf?apikey=${lolkeysapi}&img=${url}`, docname + '.pdf', '', m, false, { asDocument: true })
+}
+handler.command = /^topdf$/i
+handler.register = true
+export default handler
